1a71a108052fd06f62e509451e4d45876e83c885
Attila Boros <boros.attila17@gmail.com>
2019-12-12 17:26:51 +0100

    INSERT customers

--- database/db_schema/fill_db.sql
+++ database/db_schema/fill_db.sql
@@ -1,17 +1,17 @@
 INSERT INTO address (territory, country, city, postal_code, address_line_1, address_line_2)
-SELECT territory, country, city, postalcode, addressline1, addressline2
+SELECT DISTINCT territory, country, city, postalcode, addressline1, addressline2
 FROM sales_data_sample;
 
-INSERT INTO customer_representative (first_name, last_name, phone)
-SELECT contactfirstname, contactlastname, phone
+INSERT INTO customer_representative (phone, first_name, last_name)
+SELECT DISTINCT phone, contactfirstname, contactlastname
 FROM sales_data_sample;
 
 INSERT INTO product_category (name)
-SELECT DISTINCT productline FROM sales_data_sample;
+SELECT DISTINCT productline
+FROM sales_data_sample;
 
 INSERT INTO customer (name, customer_representative_id, address_id)
 SELECT DISTINCT customername, cr.id, a.id
-FROM sales_data_sample
-INNER JOIN address a on sales_data_sample.city = a.city
-INNER JOIN customer_representative cr on sales_data_sample.phone = cr.phone;
-
+FROM sales_data_sample sds
+         JOIN address a on sds.addressline1 = a.address_line_1
+         JOIN customer_representative cr on sds.phone = cr.phone;
