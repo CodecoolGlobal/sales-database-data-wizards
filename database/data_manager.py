608e68e9e42d254af50fd01327772488e5f0b555
Attila Boros <boros.attila17@gmail.com>
2020-01-09 15:54:29 +0100

    create connection, execute scipt with python

--- database/data_manager.py
+++ database/data_manager.py
@@ -1,51 +1,11 @@
 import os
 import psycopg2
 import psycopg2.extras
+from database_common import connection_handler
 
 
-def establish_connection(connection_data=None):
-    """
-    Create a database connection based on the :connection_data: parameter
-
-    :connection_data: Connection string attributes
-
-    :returns: psycopg2.connection
-    """
-    if connection_data is None:
-        connection_data = get_connection_data()
-    try:
-        connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
-                                                                     connection_data['user'],
-                                                                     connection_data['host'],
-                                                                     connection_data['password'])
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-    except psycopg2.DatabaseError as e:
-        print("Cannot connect to database.")
-        print(e)
-    else:
-        return conn
-
-
-def get_connection_data(db_name=None):
-    """
-    Give back a properly formatted dictionary based on the environment variables values which are started
-    with :MY__PSQL_: prefix
-
-    :db_name: optional parameter. By default it uses the environment variable value.
-    """
-    if db_name is None:
-        db_name = os.environ.get('MY_PSQL_DBNAME')
-
-    return {
-        'dbname': db_name,
-        'user': os.environ.get('MY_PSQL_USER'),
-        'host': os.environ.get('MY_PSQL_HOST'),
-        'password': os.environ.get('MY_PSQL_PASSWORD')
-    }
-
-
-def execute_script_file(file_path):
+@connection_handler
+def execute_script_file(cursor, file_path):
     """
     Execute script file based on the given file path.
     Print the result of the execution to console.
@@ -58,18 +18,17 @@ def execute_script_file(file_path):
     package_directory = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(package_directory, file_path)
     with open(full_path) as script_file:
-        with establish_connection() as conn, \
-                conn.cursor() as cursor:
-            try:
-                sql_to_run = script_file.read()
-                cursor.execute(sql_to_run)
-                print("{} script executed successfully.".format(file_path))
-            except Exception as ex:
-                print("Execution of {} failed".format(file_path))
-                print(ex.args)
+        try:
+            sql_to_run = script_file.read()
+            cursor.execute(sql_to_run)
+            print("{} script executed successfully.".format(file_path))
+        except Exception as ex:
+            print("Execution of {} failed".format(file_path))
+            print(ex.args)
 
 
-def execute_select(statement, variables=None):
+@connection_handler
+def execute_select(cursor, statement, variables=None):
     """
     Execute SELECT statement optionally parameterized
 
@@ -80,14 +39,13 @@ def execute_select(statement, variables=None):
 
     :variables:  optional parameter dict"""
     result_set = []
-    with establish_connection() as conn:
-        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-            cursor.execute(statement, variables)
-            result_set = cursor.fetchall()
+    cursor.execute(statement, variables)
+    result_set = cursor.fetchall()
     return result_set
 
 
-def execute_dml_statement(statement, variables=None):
+@connection_handler
+def execute_dml_statement(cursor, statement, variables=None):
     """
     Execute data manipulation query statement (optionally parameterized)
 
@@ -95,11 +53,9 @@ def execute_dml_statement(statement, variables=None):
 
     :variables:  optional parameter dict"""
     result = None
-    with establish_connection() as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(statement, variables)
-            try:
-                result = cursor.fetchone()
-            except psycopg2.ProgrammingError as pe:
-                pass
+    cursor.execute(statement, variables)
+    try:
+        result = cursor.fetchone()
+    except psycopg2.ProgrammingError as pe:
+        pass
     return result
